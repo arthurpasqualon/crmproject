c4d9fd936307bf9e10709b763f3f2167ed975102
bianca <bianca.camargo@acad.pucrs>
2018-07-18 23:27:56 -0300

    API node inicial - (200 ok)

---
+++ CRM-API/src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.status(200).send({ //200 =  requisição bem sucedida
+        title: "Node Express API",
+        version: "0.0.1"
+    });
+});
+
+module.exports = router;
